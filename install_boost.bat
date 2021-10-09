22c35d66a45d8f2fdce0e466db519f67d1094f54
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-10-09 08:14:52 -0400

    fix submodule update in install_boost.bat

--- install_boost.bat
+++ install_boost.bat
@@ -2,8 +2,10 @@
 
 echo cloning, updating, compiling and installing boost from source..
 
-git submodule init --recursive
+git submodule init 
 git submodule update --recursive
 cd boost
+git submodule init
+git submodule update --recursive
 bootstrap
 .\b2 install --prefix=..\install
