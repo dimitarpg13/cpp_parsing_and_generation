00a1042f8011df2dfbd9442ef5d9be119113e02e
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-10-09 08:01:26 -0400

    add batch file for installing boost

---
+++ install_boost.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo cloning, updating, compiling and installing boost from source..
+
+git submodule init
+git submodule update --recursive
+cd boost
+bootstrap
+.\b2 install --prefix=..\install
